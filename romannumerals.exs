84ef50deeadab10e5f41737274c561797c628586
Moreton Brockley <development+moretonb@esendex.com>
2015-12-26 11:24:28 +0000

    switched to using foldr instead of foldl

--- romannumerals.exs
+++ romannumerals.exs
@@ -4,7 +4,7 @@ defmodule RomanNumerals do
     end
     
     def parse_int(value, accumulator) do
-        mapping = [{1000, "M"},{900, "CM"},{500, "D"},{400, "CD"},{100, "C"},{90, "XC"},{50, "L"},{40, "XL"},{10, "X"},{9, "IX"},{5, "V"},{4, "IV"},{1, "I"}]
+        mapping = [{1, "I"},{4, "IV"},{5, "V"},{9, "IX"},{10, "X"},{40, "XL"},{50, "L"},{90, "XC"},{100, "C"},{400, "CD"},{500, "D"},{900, "CM"},{1000, "M"}]
         
         evaluateNumeral = fn
             {k,v}, accumulator when k <= value -> parse_int(value - k, accumulator <> v)
@@ -12,7 +12,7 @@ defmodule RomanNumerals do
         end
         
         if value > 0 do
-            List.foldr(mapping, accumulator, evaluateNumeral)
+            List.foldl(mapping, accumulator, evaluateNumeral)
         else
             accumulator
         end
