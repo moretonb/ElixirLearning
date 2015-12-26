1603b5e71702bee2847272bb0219e3d42950589e
Moreton Brockley <development+moretonb@esendex.com>
2015-12-26 15:56:42 +0000

    add numeral to integer

--- romannumerals.exs
+++ romannumerals.exs
@@ -1,22 +1,54 @@
 defmodule RomanNumerals do
-    def parse_int(value) do
-        parse_int(value, "")
+    @mapping [{1000, "M"},{900, "CM"},{500, "D"},{400, "CD"},{100, "C"},{90, "XC"},{50, "L"},{40, "XL"},{10, "X"},{9, "IX"},{5, "V"},{4, "IV"},{1, "I"}]
+
+    def parse_int(value), do: parse_int value, ""
+    
+    def parse_int(value, accumulator) when value > 0 do        
+        parse_int @mapping, accumulator, value
     end
     
-    def parse_int(value, accumulator) do
-        mapping = [{1, "I"},{4, "IV"},{5, "V"},{9, "IX"},{10, "X"},{40, "XL"},{50, "L"},{90, "XC"},{100, "C"},{400, "CD"},{500, "D"},{900, "CM"},{1000, "M"}]
-        
-        evaluateNumeral = fn
-            {k,v}, accumulator when k <= value -> parse_int(value - k, accumulator <> v)
-            _ -> accumulator
-        end
-        
-        if value > 0 do
-            List.foldl(mapping, accumulator, evaluateNumeral)
+    def parse_int(_, accumulator), do: accumulator
+    
+    def parse_int([{k,v}|_], accumulator, value) when k <= value do
+        parse_int value - k, accumulator <> v
+    end
+    
+    def parse_int([_|tail], accumulator, value) do
+        parse_int tail, accumulator, value
+    end
+    
+    def to_int(value), do: to_int(String.graphemes(value), 0)
+    
+    def to_int([], accumulator), do: accumulator
+    
+    def to_int([head|tail], accumulator) do
+        to_int tail, head, accumulator
+    end
+    
+    def to_int([head|tail], previous, accumulator) when previous == "" do
+        to_int tail, head, accumulator
+    end
+    
+    def to_int([head|tail], previous, accumulator) do
+        value = get_value_for_numeral previous <> head
+        if(value == 0) do
+            to_int tail, head, accumulator + get_value_for_numeral(previous)
         else
-            accumulator
+            to_int tail, "", accumulator + value
+        end        
+    end
+    
+    def to_int([], previous, accumulator) do
+        accumulator + get_value_for_numeral(previous)
+    end
+    
+    def get_value_for_numeral(numeral) do
+        case List.keyfind(@mapping, numeral, 1) do
+            {k, _} -> k
+            _ -> 0
         end
     end
 end
 
 IO.puts RomanNumerals.parse_int(1914)
+IO.puts RomanNumerals.to_int("MCMXIV")
