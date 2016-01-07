ea242696b3db3e6418c59b0fe8c5dfb66b331309
Moreton Brockley <development+moretonb@esendex.com>
2016-01-07 12:47:40 +0000

    First pass at implementing pop

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,5 +1,5 @@
 defmodule FizzBuzz do
-  def output(number) do
+  def fizzify(number) do
     case {rem(number, 3), rem(number, 5)} do
       {0,0} -> "FizzBuzz"
       {0,_} -> "Fizz"
@@ -7,8 +7,25 @@ defmodule FizzBuzz do
       _ -> number
     end
   end
+  
+  def popify(number) do
+    case {rem(number, 3), rem(number, 5), rem(number, 7)} do
+      {0,0,0} -> "FizzBuzzPop"
+      {0,0,_} -> "FizzBuzz"
+      {0,_,0} -> "FizzPop"
+      {_,0,0} -> "BuzzPop"
+      {0,_,_} -> "Fizz"
+      {_,0,_} -> "Buzz"
+      {_,_,0} -> "Pop"
+      _ -> number
+    end
+  end
 end
 
 Enum.each 1..100, fn number ->
-  IO.puts FizzBuzz.output number
+  IO.puts FizzBuzz.fizzify number
+end
+
+Enum.each 1..100, fn number ->
+  IO.puts FizzBuzz.popify number
 end
