09d3b3ea191973379544105de2b78fd5239a5e2a
Moreton Brockley <development+moretonb@esendex.com>
2016-01-11 13:09:01 +0000

    fizzbuzz feature 3 complete, removed redundant solutions

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,61 +1,24 @@
-defmodule FizzBuzz do
-  def fizzify(number) do
-    case {rem(number, 3), rem(number, 5)} do
-      {0,0} -> "FizzBuzz"
-      {0,_} -> "Fizz"
-      {_,0} -> "Buzz"
-      _ -> number
-    end
-  end
-  
-  def popify(number) do   
-    match = match_3(number) <> match_5(number) <> match_7(number)
-    case match do
-      "" -> number
-      _ -> match
-    end
-  end
-  
-  def match_3(number) when rem(number, 3) == 0 do
-    "Fizz"
-  end
-  
-  def match_3(_) do
-    ""
-  end
-  
-  def match_5(number) when rem(number, 5) == 0 do
-    "Buzz"
-  end
-  
-  def match_5(_) do
-    ""
-  end
-  
-  def match_7(number) when rem(number, 7) == 0 do
-    "Pop"
-  end
-  
-  def match_7(_) do
-    ""
-  end
-  
-  def custom(number, factor, substitution) do
+defmodule FizzBuzz do  
+  def substitute(number, {factor, substitution}) do
     case rem(number, factor) do
       0 -> substitution
-      _ -> number
+      _ -> ""
     end
+  end
+  
+  def custom(number, [head|tail], accumulator) do
+    custom(number, tail, accumulator <> substitute(number, head))
+  end
+  
+  def custom(number, [], accumulator) when accumulator == "" do
+    number
+  end
+  
+  def custom(number, [], accumulator) do
+    accumulator
   end
 end
 
 Enum.each 1..100, fn number ->
-  IO.puts FizzBuzz.fizzify number
-end
-
-Enum.each 1..100, fn number ->
-  IO.puts FizzBuzz.popify number
-end
-
-Enum.each 1..100, fn number ->
-  IO.puts FizzBuzz.custom number, 2, "Fizz"
+  IO.puts FizzBuzz.custom number, [{3, "Fizz"}, {5, "Buzz"}, {7, "Pop"}], ""
 end
