53fa29434a42f3ff08f1bd3f2f10ea078b7f5699
Moreton Brockley <development+moretonb@esendex.com>
2016-01-19 12:58:20 +0000

    Reduce complexity in fizzbuzz

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,27 +1,15 @@
 defmodule FizzBuzz do  
   def substitute(0, substitution), do: substitution
   def substitute(_, _), do: ""
-  
-  def calculate_remainder(number), do: &(rem(number, &1))
 
-  def custom(number, custom_substitutions) do
-    apply_remainder = calculate_remainder(number)
-    
-    custom(number, custom_substitutions, "", apply_remainder)
-  end
-  
-  def custom(number, [{factor, custom_substitution}|tail], accumulator, apply_remainder) do
-    remainder = apply_remainder.(factor)
-    substitution = substitute(remainder, custom_substitution)
-    new_accumulator = accumulator <> substitution
-    
-    custom(number, tail, new_accumulator, apply_remainder)
+  def custom(number, [{factor, custom_substitution}|tail], accumulator) do
+    custom(number, tail, accumulator <> substitute(rem(number, factor), custom_substitution))
   end
     
-  def custom(number, [], "", _), do: number  
-  def custom(_, [], accumulator, _), do: accumulator
+  def custom(number, [], ""), do: number  
+  def custom(_, [], accumulator), do: accumulator
 end
 
 Enum.each 1..100, fn number ->
-  IO.puts FizzBuzz.custom number, [{3, "Fizz"}, {5, "Buzz"}, {7, "Pop"}]
+  IO.puts FizzBuzz.custom number, [{3, "Fizz"}, {5, "Buzz"}, {7, "Pop"}], ""
 end
