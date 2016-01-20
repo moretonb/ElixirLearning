536b9cdca6747edd378ae19b41b3f00c2708fb02
Moreton Brockley <development+moretonb@esendex.com>
2016-01-20 14:11:27 +0000

    refactor fizzbuzz to pipe functions and use foldr

--- fizzbuzz.exs
+++ fizzbuzz.exs
@@ -1,15 +1,25 @@
 defmodule FizzBuzz do  
+  def apply_rules(number, rules) do
+    apply_rule_for_number = fn n -> fn (r, a) -> apply_rule(r, a, n) end end
+  
+    rules
+    |> List.foldr("", apply_rule_for_number.(number))
+    |> generate_output(number)
+    |> IO.puts
+  end
+  
+  defp apply_rule({factor, custom_substitution}, accumulator, number) do
+    accumulator <> substitute(rem(number, factor), custom_substitution)
+  end
+  
   defp substitute(0, substitution), do: substitution
   defp substitute(_, _), do: ""
-
-  def custom(number, [{factor, custom_substitution}|tail], accumulator) do
-    custom(number, tail, accumulator <> substitute(rem(number, factor), custom_substitution))
-  end
-    
-  def custom(number, [], ""), do: number  
-  def custom(_, [], accumulator), do: accumulator
+  
+  defp generate_output("", number), do: number
+  defp generate_output(substitute, _), do: substitute
 end
 
-Enum.each 1..100, fn number ->
-  IO.puts FizzBuzz.custom number, [{3, "Fizz"}, {5, "Buzz"}, {7, "Pop"}], ""
-end
+rules = [{3, "Fizz"}, {5, "Buzz"}, {7, "Pop"}]
+
+1..100
+|> Enum.each(&(FizzBuzz.apply_rules &1, rules))
