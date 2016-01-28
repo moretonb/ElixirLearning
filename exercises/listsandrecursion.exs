5d49476976d0b84b98828e6533cf664b7b146d0c
Moreton Brockley <development+moretonb@esendex.com>
2016-01-28 13:10:31 +0000

    solve listsandrecursion-2

--- exercises/listsandrecursion.exs
+++ exercises/listsandrecursion.exs
@@ -8,4 +8,13 @@ defmodule ListOperations do
   #ListsAndRecursion-1
   def mapsum([], _), do: 0
   def mapsum([head|tail], function), do: function.(head) + mapsum(tail, function)
+  
+  #ListsAndRecursion-2
+  def max([]), do: nil
+  def max([head|tail]) do
+    case max(tail) do
+      value when value > head and value !== nil -> value
+      _ -> head
+    end
+  end
 end
