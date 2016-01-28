0c7ee83e7cb25a4aa049913ba2825b08c4056a01
Moreton Brockley <development+moretonb@esendex.com>
2016-01-28 12:55:11 +0000

    solve listsandrecursion-1

--- exercises/listsandrecursion.exs
+++ exercises/listsandrecursion.exs
@@ -3,3 +3,9 @@ defmodule Math do
   def sum([]), do: 0
   def sum([head|tail]), do: head + sum(tail)
 end
+
+defmodule ListOperations do
+  #ListsAndRecursion-1
+  def mapsum([], _), do: 0
+  def mapsum([head|tail], function), do: function.(head) + mapsum(tail, function)
+end
